4d86f171c9543aaf5d285da4ba7d89587fb1cb74
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-11-15 15:39:42 -0800

    limited to 50000

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   def self.csv
     users = ["first name", "last name", "email", "opted in?", "user creation date", "company", "venue", "company creation date"].to_csv
-    User.includes(:company).find_each do |user|
+    User.all.includes(:company).limit(50000).each do |user|
       users << "#{user.first_name},#{user.last_name},#{user.email},#{user.opt_in},#{user.created_at},#{user.company.name},#{user.company.venue},#{user.company.created_at}\n"
     end
     users
