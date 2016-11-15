513e94f56da975ba086c1358884dd728c8f86e46
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-11-15 15:14:44 -0800

    used outside database and optimized for speed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
 
   def self.csv
     users = ["first name", "last name", "email", "opted in?", "user creation date", "company", "venue", "company creation date"].to_csv
-    User.all.includes(:company).limit(10).each do |user|
-      users += [user.first_name, user.last_name, user.email, user.opt_in, user.created_at, user.company.name, user.company.venue, user.company.created_at].to_csv
+    User.all.includes(:company).each_with_index do |user, idx|
+      users << "#{user.first_name},#{user.last_name},#{user.email},#{user.opt_in},#{user.created_at},#{user.company.name},#{user.company.venue},#{user.company.created_at}\n"
     end
     users
   end
