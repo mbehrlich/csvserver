fe7989d97820e6b1105221697c04bfac089a942a
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-11-16 15:40:46 -0800

    another attempt at fixing memory leak

--- app/controllers/csvservers_controller.rb
+++ app/controllers/csvservers_controller.rb
@@ -6,14 +6,14 @@ class CsvserversController < ApplicationController
   def root
     # @users = User.csv
     # send_data @users, filename: "users-#{Date.today}.csv"
-    p response_body
     response.headers['Content-Type'] = 'text/csv'
     response.headers['Content-Disposition'] = "inline; filename='users-#{Date.today}.csv'"
     response.stream.write ["first name", "last name", "email", "opted in?", "user creation date", "company", "venue", "company creation date"].to_csv
     User.includes(:company).find_each do |user|
-      response.stream.write "#{user.first_name},#{user.last_name},#{user.email},#{user.opt_in},#{user.created_at},#{user.company.name},#{user.company.venue},#{user.company.created_at}\n"
+      temp_string = "#{user.first_name},#{user.last_name},#{user.email},#{user.opt_in},#{user.created_at},#{user.company.name},#{user.company.venue},#{user.company.created_at}\n"
+      response.stream.write temp_string
     end
-    GC.start
+
     response.stream.close
   end
 
