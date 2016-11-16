14559da3f0c00123b39631b25f4a65de51009008
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-11-16 14:09:10 -0800

    Switched to streaming delivery

--- app/controllers/csvservers_controller.rb
+++ app/controllers/csvservers_controller.rb
@@ -1,10 +1,18 @@
 require 'csv'
 
 class CsvserversController < ApplicationController
+  include ActionController::Live
 
   def root
-    @users = User.csv
-    send_data @users, filename: "users-#{Date.today}.csv"
+    # @users = User.csv
+    # send_data @users, filename: "users-#{Date.today}.csv"
+    response.headers['Content-Type'] = 'text/csv'
+    response.headers['Content-Disposition'] = "inline; filename='users-#{Date.today}.csv'"
+    response.stream.write ["first name", "last name", "email", "opted in?", "user creation date", "company", "venue", "company creation date"].to_csv
+    User.includes(:company).find_each do |user|
+      response.stream.write "#{user.first_name},#{user.last_name},#{user.email},#{user.opt_in},#{user.created_at},#{user.company.name},#{user.company.venue},#{user.company.created_at}\n"
+    end
+    response.stream.close
   end
 
 end
